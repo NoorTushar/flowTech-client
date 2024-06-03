b84898e76aff07b714982894a48b7c8c4295edbc
NoorTushar <tushar.noor.khan@gmail.com>
2024-06-03 20:21:05 +0600

    dashboard layout created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import PrivateRoute from "./PrivateRoute";
+import DashboardLayout from "../Layouts/DashboardLayout";
 
 export const router = createBrowserRouter([
    {
@@ -22,5 +24,13 @@ export const router = createBrowserRouter([
             element: <Register />,
          },
       ],
+   },
+   {
+      path: "/dashboard",
+      element: (
+         <PrivateRoute>
+            <DashboardLayout />
+         </PrivateRoute>
+      ),
    },
 ]);
