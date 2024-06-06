fb5b5f51034d754e98ae9bc73843f2fc0edb31e4
NoorTushar <tushar.noor.khan@gmail.com>
2024-06-06 19:15:30 +0600

    fitler by name function is working on progress page

--- src/Pages/Dashboard/HR/Progress/Progress.jsx
+++ src/Pages/Dashboard/HR/Progress/Progress.jsx
@@ -1,10 +1,38 @@
 import { format } from "date-fns";
 import LoadingSpinner from "../../../../Components/Shared/LoadingSpinner";
 import Title from "../../../../Components/Shared/Title/Title";
-import useAllWorks from "../../../../Hooks/useAllWorks";
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 
 const Progress = () => {
-   const [allWorks, uniqueNames, isLoading] = useAllWorks();
+   // const [allWorks, uniqueNames, isLoading] = useAllWorks();
+   const axiosSecure = useAxiosSecure();
+   const [selectedName, setSelectedName] = useState("");
+   const [currentName, setCurrentName] = useState("");
+
+   const { data, isLoading } = useQuery({
+      queryKey: ["allWorks", selectedName],
+      queryFn: async () => {
+         const { data } = await axiosSecure(`/works/?name=${selectedName}`);
+         console.log(data);
+         return data;
+      },
+   });
+
+   const handleFilterName = (e) => {
+      e.preventDefault();
+      setSelectedName(e.target.value);
+   };
+
+   useEffect(() => {
+      setCurrentName(selectedName);
+   }, [selectedName]);
+
+   console.log("selected Name:", selectedName);
+   console.log("current name", currentName);
+   const allWorks = data?.works || [];
+   const uniqueNames = data?.uniqueNames || [];
 
    if (isLoading) return <LoadingSpinner />;
    console.log(allWorks, uniqueNames);
@@ -18,7 +46,11 @@ const Progress = () => {
          />
          {/* Filter Fields */}
          <div>
-            <select name="filterName">
+            <select
+               name="filterName"
+               value={currentName}
+               onChange={handleFilterName}
+            >
                <option value="">filter by name</option>
                {uniqueNames.map((uname, index) => (
                   <option key={index} value={uname.employeeName}>
